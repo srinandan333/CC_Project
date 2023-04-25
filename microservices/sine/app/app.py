d0371794804daf59bb7ca9ca941dcbf47ed28495
srinandan333 <111896094+srinandan333@users.noreply.github.com>
2023-04-25 11:48:07 +0530

    Add files via upload

---
+++ microservices/sine/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+from flask_restful import Api, Resource
+from math import sin
+
+class Sine(Resource): 
+	def get(self, num1, num2):
+		return {'result': sin(int(num1))}
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Sine, '/<num1>/<num2>')
+
+if __name__ =="__main__":
+	app.run(
+		debug=True,
+		port=5064,
+		host="0.0.0.0"
+	)
